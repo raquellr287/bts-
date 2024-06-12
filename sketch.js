f3c8cc22ff85b0d0c7873450e9b83e607b56e2e4
raquellr287 <raquel.lr287@gmail.com>
2024-06-12 13:29:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let palavras;
+function setup() {
+  createCanvas(400, 400)
+ palavra=palavraaleatoria(); 
+}
+
+function palavrasaleatoria(){
+let palavras = ["raquel","nicolly" ,"gael"]
+return random(palavras);
+}
+function iniciadoresCores()
+ {
+  background("yellow");
+ fill("black");
+   textSize(64);
+  textAlign(CENTER,CENTER);
+}
+
+
+
+
+  
+  
